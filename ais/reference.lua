f11e5c79dc561e7b7be2cedf14ca968c568c43a0
Rich Lane <rlane@club.cc.cmu.edu>
2012-02-19 11:29:15 -0800

    reference ai: support ion beams

--- ais/reference.lua
+++ ais/reference.lua
@@ -1,3 +1,6 @@
+local my_class = class
+local my_ship = ships[my_class]
+
 function find_target()
 	for i, t in ipairs(sensor_contacts({})) do
 		if t:team() ~= team then
@@ -26,10 +29,18 @@ while true do
 		local tp = target:position_vec()
 		debug_square(tp.x, tp.y, 20)
 		drive_towards(100, tp.x, tp.y)
-		if check_gun_ready(0) then
-			local a = lead_vec(position_vec(), target:position_vec(), velocity_vec(), target:velocity_vec(), 3000, 10)
-			if a then
-				fire_gun(0, a)
+
+		if class == "ion_cannon_frigate" then
+			local p = position_vec()
+			if p:distance(tp) < my_ship.guns.main.length then
+				fire("main", heading())
+			end
+		else
+			if check_gun_ready("main") then
+				local a = lead_vec(position_vec(), target:position_vec(), velocity_vec(), target:velocity_vec(), 3000, 10)
+				if a then
+					fire("main", a)
+				end
 			end
 		end
 	else
